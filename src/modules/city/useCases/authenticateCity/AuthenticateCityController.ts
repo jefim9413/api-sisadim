d5f35eee1acac3974a98e0df68ece0af36f4fdeb
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-05-10 11:02:46 -0300

    finalizando controller de Authenticação

---
+++ src/modules/city/useCases/authenticateCity/AuthenticateCityController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+import { AuthenticateUseCase } from './AuthenticateCityUseCase'
+
+export class AuthenticateController {
+  async handle(request: Request, response: Response) {
+    const { name, password } = request.body
+    const authenticateUseCase = container.resolve(AuthenticateUseCase)
+    const token = await authenticateUseCase.execute({
+      password,
+      name,
+    })
+
+    return response.json(token)
+  }
+}
