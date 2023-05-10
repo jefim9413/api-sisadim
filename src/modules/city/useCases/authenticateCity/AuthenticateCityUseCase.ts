01c8455975523e81c7ecb83469fa6c5a199121da
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-05-10 11:03:04 -0300

    terminado useCase de authenticação

--- src/modules/city/useCases/authenticateCity/AuthenticateCityUseCase.ts
+++ src/modules/city/useCases/authenticateCity/AuthenticateCityUseCase.ts
@@ -1,9 +1,17 @@
 import { inject, injectable } from 'tsyringe'
 import { ICityRepository } from '../../repositories/ICityRepository'
+import { sign } from 'jsonwebtoken'
 
 interface ICityRequest {
   name: string
   password: string
+}
+
+interface ICityResponse {
+  city: {
+    name: string
+  }
+  token: string
 }
 
 @injectable()
@@ -13,5 +21,25 @@ export class AuthenticateUseCase {
     private cityRepository: ICityRepository,
   ) {}
 
-  async execute({ name, password }: ICityRequest) {}
+  async execute({ name, password }: ICityRequest) {
+    const city = await this.cityRepository.findByCity(name)
+
+    if (!city) {
+      throw new Error('ciy or password not found')
+    }
+
+    const token = sign({}, 'ac81a9ff47be796b7f2e4ccad808e14a', {
+      subject: city.id,
+      expiresIn: '99d',
+    })
+
+    const tokenreturn: ICityResponse = {
+      token,
+      city: {
+        name: city.name,
+      },
+    }
+
+    return tokenreturn
+  }
 }
