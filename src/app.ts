2cc4f46332e772aa8fd12cfa4492f54d020d8da9
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-05-10 09:03:57 -0300

    separando app do server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { env } from './env'
-import { app } from './server'
+import { app } from './shared/infra/server'
 
 app.listen(env.PORT, () => {
   console.log('🚀 Running Server 🚀')
--- src/shared/infra/server.ts
+++ src/shared/infra/server.ts
@@ -1,9 +1,13 @@
+import 'reflect-metadata'
 import 'express-async-errors'
 import express, { NextFunction, Request, Response } from 'express'
+import '../container'
+import { router } from './routes'
 
 export const app = express()
 app.use(express.json())
 
+app.use(router)
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof Error) {
