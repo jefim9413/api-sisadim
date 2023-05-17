926cd28527a12675779c2bf499971e999734c3e6
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-05-16 23:29:00 -0300

    adicionando consulta por id

--- src/modules/city/repositories/ICityRepository.ts
+++ src/modules/city/repositories/ICityRepository.ts
@@ -2,4 +2,5 @@ import { City, Prisma } from '@prisma/client'
 export interface ICityRepository {
   create(data: Prisma.CityCreateInput): Promise<City>
   findByCity(name: string): Promise<City>
+  findById(id: string): Promise<City>
 }
