a2ad5b3ad3099b869477442715468bbf50558a04
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-05-19 16:29:00 -0300

    adicionando rotas de instituição

--- src/shared/infra/routes/index.ts
+++ src/shared/infra/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { cityRouter } from './city.routes'
 import { autheenticaterRoutes } from './Authenticate.routes'
+import { institutionRouter } from './institution.routes'
 
 export const router = Router()
 
 router.use('/city', cityRouter)
+router.use('/institution', institutionRouter)
 router.use(autheenticaterRoutes)
