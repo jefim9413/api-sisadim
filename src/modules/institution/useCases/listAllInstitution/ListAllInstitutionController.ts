9e2a19c18054ec3aa05d7a5796c330bceb4774aa
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-05-19 16:33:36 -0300

    Controller de listar todas as instituições

---
+++ src/modules/institution/useCases/listAllInstitution/ListAllInstitutionController.ts
@@ -0,0 +1,15 @@
+import { container } from 'tsyringe'
+import { ListAllInstitutionUseCase } from './ListAllInstitutionUseCase'
+import { Request, Response } from 'express'
+
+export class ListAllInstitutionController {
+  async handle(request: Request, response: Response) {
+    const listAllInstitutionUseCase = container.resolve(
+      ListAllInstitutionUseCase,
+    )
+    const city_id = request.city.id
+
+    const institution = await listAllInstitutionUseCase.execute(city_id)
+    return response.status(201).send(institution)
+  }
+}
