f053cfeec8111858becde7bb3698b993ab76a5c1
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-05-22 18:21:25 -0300

    adicionando as rotas de turma

--- src/shared/infra/routes/index.ts
+++ src/shared/infra/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express'
 import { cityRouter } from './city.routes'
 import { autheenticaterRoutes } from './Authenticate.routes'
 import { institutionRouter } from './institution.routes'
+import { turmaRouter } from './turma.routes'
 
 export const router = Router()
 
 router.use('/city', cityRouter)
 router.use('/institution', institutionRouter)
+router.use('/turma', turmaRouter)
 router.use(autheenticaterRoutes)
