76c6c38515ffd2b69abd8a07d4bc47278d589b9d
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-05-27 13:33:19 -0300

    finalidando interface de Repository de Geral

--- src/modules/general/repositories/IGeneralRepostitory.ts
+++ src/modules/general/repositories/IGeneralRepostitory.ts
@@ -2,6 +2,6 @@ import { General, Prisma } from '@prisma/client'
 
 export interface IGeneralRepository {
   create(data: Prisma.GeneralCreateManyInput): Promise<General>
-  findById(id: string): Promise<General>
+  findById(id: string): Promise<General | null>
   findAll(): Promise<General[]>
 }
